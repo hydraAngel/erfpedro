b51ae9f53249ccc1758e42e65c7da4e04628aeaf
hydraAngel <ppvverissimo@gmail.com>
2024-05-22 22:39:48 -0300

    arrumei um pouco o update checker

--- checkUpdate.py
+++ checkUpdate.py
@@ -3,21 +3,25 @@ import zipfile
 import os
 import shutil
 
+
 def get_latest_release_info():
     url = "https://api.github.com/repos/hydraangel/erfpedro/releases/latest"
     response = requests.get(url)
     response.raise_for_status()  # Check if the request was successful
     return response.json()
 
+
 def check_for_updates():
     latest_release = get_latest_release_info()
     latest_version = latest_release["tag_name"]
     update_url = latest_release["assets"][0]["browser_download_url"]
-    with open('versaoatual.txt', r) as ver:
+    with open("versaoatual.txt", 'r') as ver:
         client_version = ver.read()
+    print(client_version, latest_version)
     if latest_version != client_version:
         return latest_version, update_url
     return None, None
+
 
 def download_update(update_url, download_path="ERF.zip"):
     response = requests.get(update_url, stream=True)
@@ -26,6 +30,9 @@ def download_update(update_url, download_path="ERF.zip"):
             file.write(chunk)
     return download_path
 
+
 def install_update(zip_path, extract_to="update"):
-    
+
     print("Update installed successfully. Please restart the application.")
+
+print(check_for_updates())
